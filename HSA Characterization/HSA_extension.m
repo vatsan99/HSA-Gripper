ea241a57589a8d1e02d19cc24b7d5e5326ba7100
Srivatsan <sri.vatsan0307@gmail.com>
2024-03-22 13:00:02 -0700

    Modified .m

--- HSA Characterization/HSA_extension.m
+++ HSA Characterization/HSA_extension.m
@@ -1,23 +1,26 @@
 clc
 close all
 
-% data = readtable('HSA-Tensile-Torsion_1.csv');
-% data1 = table2array(data);
-% data2 = table2array(readtable('HSA-Tensile-Torsion_2.csv'));
 
-HSA_FEA = table2array(readtable('HSA-FEA-Extension.csv'));
-HSA_exp = table2array(readtable('HSA-FEA-Extension.csv'));
+HSA_FEA = table2array(readtable('.\HSA-FEA-Extension.csv')); % FEA data
+HSA_exp = table2array(readtable('.\HSA-full-extension-test.is_tcyclic_tao_Exports\HSA-full-extension-test_1.csv')); % Instron data
+HSA_exp2 = table2array(readtable('.\HSA-full-extension-test.is_tcyclic_tao_Exports\HSA-full-extension-test_2.csv')); % Instron data
 
-HSA_exp_disp = HSA_exp(:, 1);
-HSA_exp_force = HSA_exp(:, 2);
+HSA_exp_disp = HSA_exp(:, 2);
+HSA_exp_force = HSA_exp(:, 3).*1000; % force data to N
 
-HSA_FEA_disp = HSA_FEA(:, 2);
-HSA_FEA_force = HSA_FEA(:, 3);
+HSA_exp_disp2 = HSA_exp2(:, 2);
+HSA_exp_force2 = HSA_exp2(:, 3).*1000; % force data to N
+
+HSA_FEA_disp = HSA_FEA(:, 1);
+HSA_FEA_force = HSA_FEA(:, 2);
 
 figure()
-plot(HSA_exp_disp, HSA_exp_force, "LineWidth", 1.5)
+plot(HSA_exp_disp, HSA_exp_force, "LineWidth", 1.5) % instron extension
 hold on
-plot(HSA_FEA_disp, HSA_FEA_force, "LineWidth", 1.5)
+plot(HSA_exp_disp2, HSA_exp_force2, "LineWidth", 1.5)
+hold on
+plot(HSA_FEA_disp, HSA_FEA_force, "LineWidth", 1.5) % FEA
 
 xlabel('Displacement [mm]')
 ylabel('Force [N]')
