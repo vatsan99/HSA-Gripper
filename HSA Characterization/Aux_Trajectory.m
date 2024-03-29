edd36eeefb1a3fcd23bc2ab45ea1cdc38e31eeec
Srivatsan <sri.vatsan0307@gmail.com>
2024-03-29 12:17:58 -0700

    Files Modified

--- HSA Characterization/Aux_Trajectory.m
+++ HSA Characterization/Aux_Trajectory.m
@@ -1,7 +1,10 @@
 close all
 clc
 
-points = [0, 60; 200, 0]; % maximum extension and rotation
+max_disp = 45.34;
+max_theta = 70.42;
+
+points = [0, max_disp; max_theta, 0]; % maximum extension and rotation
 
 figure;
 
