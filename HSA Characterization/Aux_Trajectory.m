8efbe2f636441f3e08fbbba97037761894bd96e7
Srivatsan <sri.vatsan0307@gmail.com>
2024-03-28 21:45:06 -0700

    Files Modified

--- HSA Characterization/Aux_Trajectory.m
+++ HSA Characterization/Aux_Trajectory.m
@@ -15,10 +15,21 @@ y = 0.2512 * x;
 hold on
 
 p2 = plot(x, y, 'k-', 'LineWidth', 1.5, 'DisplayName', 'Auxetic Trajectory');
-legend([p1, p2], {'Points', 'Line'}, 'TextColor', 'auto');
+
+% Defining parallel lines
+
+y1 = 0.2512.*x(:,250) + 70;
+
+hold on
+plot(x(:,250), y1, 'g-', 'LineWidth', 1.5, 'DisplayName', 'Parallel Line 1')
+
+
+
+
+% legend([p1, p2], {'Points', 'Line'}, 'TextColor', 'auto');
 
 xlabel('Rotation [\theta]')
 ylabel('Extension [mm]')
 
-xlim([0 250]);
-ylim([0 250]);
+% xlim([0 250]);
+% ylim([0 250]);
