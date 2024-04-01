2be1f2da696e15826ffaccdc73063d575d32dc96
Srivatsan <sri.vatsan0307@gmail.com>
2024-03-31 20:19:33 -0700

    Files Modified

--- HSA Characterization/PerformanceLandscape/HeatMap_SectionalHSA.m
+++ HSA Characterization/PerformanceLandscape/HeatMap_SectionalHSA.m
@@ -1,10 +1,10 @@
 close all
 clc
 
-data = readtable(".\PerformanceLandscape\0to35mm_0to130deg_NRGLandscape_1.csv"); % processed raw data
+data = readtable(".\0to35mm_0to130deg_NRGLandscape_1.csv"); % processed raw data: Single HSA
 data = table2array(data);
 
-theta = data(:, 4); % x
+theta = data(:, 4).*(-1); % x
 disp = data(:, 2); % y
 
 force_response = data(:, 3); % z
@@ -34,7 +34,7 @@ title({'Performance Heat Map';'F = f(y, \theta)'});
 xlabel('Rotation, \theta [degrees]');
 ylabel('Extension, y [mm]');
 zlabel('Force [mN]');
-grid off
+grid on
 view(0, 90)
 
 x0 = 950;
@@ -62,3 +62,5 @@ y0 = 350;
 width = 510;
 height = 380;
 set(gcf, 'position', [x0, y0, width, height])
+
+% Double / Sectional HSA
