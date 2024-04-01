0c5196a83a3307220dd51cc2ebe687278113087f
Srivatsan <sri.vatsan0307@gmail.com>
2024-03-31 22:55:01 -0700

    Files Modified

--- HSA Characterization/PerformanceLandscape/HeatMap_SectionalHSA.m
+++ HSA Characterization/PerformanceLandscape/HeatMap_SectionalHSA.m
@@ -10,3 +10,6 @@ theta_sectional_HSA = table2array(data_sectional_HSA(:, 4).*(-1)); % x
 
 force_sectional_HSA = table2array(data_sectional_HSA(:, 3)); % z
 torque_sectional_HSA = table2array(data_sectional_HSA(:, 5)); % z
+
+figure()
+scatter3(data_sectional_HSA, theta_sectional_HSA, force_sectional_HSA)
