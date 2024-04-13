652d0f5eaa5c7cfbeb2abe6985b1bf5ea13f69f6
Srivatsan <sri.vatsan0307@gmail.com>
2024-04-13 15:15:59 -0700

    .m Modified

--- FPU50 Characterization/PA6-FEA-Validation.py
+++ FPU50 Characterization/PA6-FEA-Validation.py
@@ -4,19 +4,16 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('D:\Srivatsan\HSA-gripper-files\FPU50 Characterization\FEA-PA6-tensile.csv')
-# data
 
 force_data = data['Force1']
 disp_data = data['Displacement1']
-# print(disp_data)
-# print(force_data)
 
 specimen_thickness = 3.4 # in mm
 specimen_width = 6 # in mm
 original_length = 33 # in mm - distance between the grips
 
 new_length = disp_data + original_length
-# print("New length: ", new_length)
+
 
 def stress_strain(force, displacement):
     area = specimen_thickness * specimen_width # cross sectional area of the specimen
@@ -29,9 +26,6 @@ def stress_strain(force, displacement):
     return stress, strain
 
 stress, strain = stress_strain(force_data, disp_data)
-
-# print(stress)
-# print(strain)
 
 stress_strain_data = {'Stress':stress, 'Strain': strain}
 df = pd.DataFrame(stress_strain_data) # convert stress-strain data to a dataframe
--- FPU50 Characterization/dog-bone-analysis-FPU50.py
+++ FPU50 Characterization/dog-bone-analysis-FPU50.py
@@ -15,13 +15,13 @@ data1 = specimen1[specimen1['Displacement'] < 1]
 data2 = specimen2[specimen2['Displacement'] < 1]
 data3 = specimen3[specimen3['Displacement'] < 1]
 
-
 # plotting the data
 
 plt.figure()
 plt.plot(data1['Displacement'], data1['Force'])
 plt.plot(data2['Displacement'], data2['Force'])
 plt.plot(data3['Displacement'], data3['Force'])
+
 plt.title("Force Displacement Curve")
 plt.xlabel("Displacement $[mm]$")
 plt.ylabel("Force $[N]$")
@@ -30,14 +30,8 @@ plt.legend(['Specimen 1', 'Specimen 2', 'Specimen 3'])
 
 specimen_thickness = 3.4 # in mm
 specimen_width = 6 # in mm
-# original_length = 65 # in mm
 
-# new_length = disp1 + original_length
-# new_length
-# new_data = instron_data[instron_data['Displacement1'] < 1] # displacement less than 1 mm
-# new_data
-# new_force_data = new_data['Force1']
-# new_disp_data = new_data['Displacement1']
+
 def stress_strain(force, displacement):
     area = specimen_thickness * specimen_width # cross sectional area of the specimen
 
@@ -50,14 +44,7 @@ stress1, strain1 = stress_strain(data1['Force'], data1['Displacement'])
 stress2, strain2 = stress_strain(data2['Force'], data2['Displacement'])
 stress3, strain3 = stress_strain(data3['Force'], data3['Displacement'])
 
-# stress_strain_data = {'Stress':stress, 'Strain': strain}
-# df = pd.DataFrame(stress_strain_data)
-# df
 
-
-# Youngs_modulus_N = (df.iloc[-1]['Stress'] - df.iloc[0]['Stress']) / (df.iloc[-1]['Strain'] - df.iloc[0]['Strain'])
-
-# print("Young's Modulus (in MPa) = ", Youngs_modulus_N)
 plt.figure()
 plt.plot(strain1, stress1)
 plt.plot(strain2, stress2)
@@ -93,6 +80,7 @@ new_force_data = new_data['Force1']
 new_disp_data = new_data['Displacement1']
 
 new_data
+
 def stress_strain_FEA(force, displacement):
     area = specimen_thickness * specimen_width # cross sectional area of the specimen
 
@@ -103,8 +91,6 @@ def stress_strain_FEA(force, displacement):
 
 stress, strain = stress_strain_FEA(new_force_data, new_disp_data)
 
-# print(stress)
-# print(strain)
 
 stress_strain_data = {'Stress':stress, 'Strain': strain}
 df = pd.DataFrame(stress_strain_data)
@@ -118,9 +104,8 @@ print("ASTM Type IV: Young's Modulus (in MPa) = ", model.coef_[0])
 # stress_strain_data
 plt.figure()
 plt.plot(disp_data, force_data, label = 'FEA data')
-# plt.plot(specimen1['Displacement'], specimen1['Force'], label = 'Specimen 1')
 plt.plot(specimen2['Displacement'], specimen2['Force'], label = 'Specimen 2')
-# plt.plot(specimen3['Displacement'], specimen3['Force'], label = 'Specimen 3')
+
 plt.title("Force Displacement Curve - FPU50")
 plt.xlabel("Displacement $[mm]$")
 plt.ylabel("Force $[N]$")
@@ -129,9 +114,8 @@ plt.show()
 
 plt.figure()
 plt.plot(strain2, stress2, label = 'Specimen')
-# plt.plot(specimen1['Displacement'], specimen1['Force'], label = 'Specimen 1')
 plt.plot(df['Strain'], df['Stress'], label = 'FEA data')
-# plt.plot(specimen3['Displacement'], specimen3['Force'], label = 'Specimen 3')
+
 plt.title("Stress Strain Curve - FPU50")
 plt.xlabel("Strain $[-]$")
 plt.ylabel("Stress $[N/mm^2]$")
@@ -139,3 +123,7 @@ plt.legend()
 plt.show()
 
 # df.to_csv('stress-strain-PA6-FEA.csv')
+
+'''
+This is a comment block
+'''
