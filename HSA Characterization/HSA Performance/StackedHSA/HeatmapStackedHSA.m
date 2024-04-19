eca2b4cabbbf3e4a9703db46e3a0d778bb147521
Srivatsan <sri.vatsan0307@gmail.com>
2024-04-18 19:06:16 -0700

    .m Updated

--- HSA Characterization/HSA Performance/StackedHSA/HeatmapStackedHSA.m
+++ HSA Characterization/HSA Performance/StackedHSA/HeatmapStackedHSA.m
@@ -5,7 +5,7 @@ run('SpringConstantMatrixGripper.m')
 
 %{
     Run SpringConstantMatrixGripper.m to process raw Instron test data. This script utilizes processed data to plot
-    surface plots to generate a heatmap for force, torque, and spring constant (average) for the stacked (4 x 2) HSA.
+    surface plots to generate a heatmap for force, torque, and spring constant (average) for the Stacked HSA.
 %}
 
 ticklabels = {'0', '30', '60', '90', '120'};
@@ -26,11 +26,16 @@ rotMatrix = reshape(theta(indMat(:,1)), length(dispSteps), length(rotSteps));
 
 % Plot contour for Mean Force
 
+figure()
 subplot(1, 3, 1)
 surfc(thetaMat, dispMat, fMeanMat, 'EdgeAlpha', 0)
-colorbar;
-clim([-2e4 2e4]);
-title({'Stacked HSA';'Mean Force'}, 'FontSize', 10)
+hcb = colorbar;
+% hcb.Position = hcb.Position + [0.075 0 0 0];
+title_handle = get(hcb, 'Title');
+title_string = {'F [N]'};
+set(title_handle ,'String', title_string);
+clim([-15 30]);
+title({'Stacked HSA';'Mean Force (F)'}, 'FontSize', 10)
 view(0, 90)
 xticks(tl)
 xticklabels(ticklabels)
@@ -42,37 +47,45 @@ box on
 
 subplot(1, 3, 2)
 surfc(thetaMat, dispMat, tauMeanMat, 'EdgeAlpha', 0)
-colorbar;
-clim([-50 65]);
-title({'Stacked HSA';'Mean Torque'}, 'FontSize', 10)
+hcb = colorbar;
+% hcb.Position = hcb.Position + [0.075 0 0 0];
+title_handle = get(hcb, 'Title');
+title_string = {'\tau [Nmm]'};
+set(title_handle ,'String', title_string);
+clim([-60 75]);
+title({'Stacked HSA';'Mean Torque (\tau)'}, 'FontSize', 10)
 view(0, 90)
 xticks(tl)
 xticklabels(ticklabels)
 ylabel('Extension [mm]')
 xlabel('Rotation, \theta [\circ]')
 box on
-% Plot Spring Constant as a funtcion of rotation and displacement
+
+% Plot Spring Constant as a function of rotation and displacement
 
 subplot(1, 3, 3)
 surfc(rotMatrix, dispMat, kFitMatrix, 'EdgeAlpha', 0);
+hcb = colorbar;
+% hcb.Position = hcb.Position + [0.075 0 0 0];
+title_handle = get(hcb, 'Title');
+title_string = {'k [N/mm]'};
+set(title_handle ,'String', title_string);
+clim([0 5]);
+title({'Stacked HSA';'Mean Spring Constant (k)'}, 'FontSize', 10);
 view(0, 90)
-colorbar; % Adds a colorbar to indicate the values
-clim([0 4500]);
 xticks(tl)
 xticklabels(ticklabels)
-title({'Stacked HSA';'Spring Constant'}, 'FontSize', 10);
 ylabel('Extension [mm]')
 xlabel('Rotation, \theta [\circ]')
 box on
 
 colormap("jet")
 
-
 % figure size
 
 x0 = 900;
 y0 = 410;
-width = 1200;
+width = 1250;
 height = 300;
 set(gcf, 'position', [x0, y0, width, height])
 exportgraphics(gcf, 'D:\Srivatsan\HSA-gripper-files\Plot Images\HeatmapStacked.png', 'Resolution', 800)
