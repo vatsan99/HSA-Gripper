efbdc635fad48af95921e4fda64955f03b9e82c4
Srivatsan <sri.vatsan0307@gmail.com>
2024-04-28 13:31:08 -0700

    GripForce.m Updated

--- Grip Force Test/GripForce.m
+++ Grip Force Test/GripForce.m
@@ -7,22 +7,28 @@ clc
     Antipodal Grasping: Using opposing forces to grasp the cylinder (opposite points on the surface of the test cylinder)
 %}
 
-imported_data = {'GripforceTestSample_4-NoEcoflexLayer.csv', 'GripforceTestSample_8-PinchGrasp2.csv', 'GripforceTestSample_7-CagingGrasp2'};
-legend_labels = {'Pinch Grasp: No Ecoflex', 'Pinch Grasp', 'Caging Grasp'};
+% imported_data = {'GripforceTestSample_4-NoEcoflexLayer.csv', 'GripforceTestSample_8-PinchGrasp2.csv', 'GripforceTestSample_7-CagingGrasp2'};
+% legend_labels = {'Pinch Grasp: No Ecoflex', 'Pinch Grasp', 'Caging Grasp'};
 
+imported_data = {'GripForceTest-Apr28/GripforceTestSample-90degPinch.csv'
+                'GripForceTest-Apr28/GripforceTestSample-120degPinch.csv'
+                'GripForceTest-Apr28/GripforceTestSample-90degCaging.csv'
+                'GripForceTest-Apr28/GripforceTestSample-120Caging.csv'};
+
+legend_labels = {'Pinch Grasp (90\circ)', 'Pinch Grasp (120\circ)', 'Caging Grasp (90\circ)', 'Caging Grasp (120\circ)'};
 
 figure()
 
 % plot zero line
 x = 0:99;
 y = zeros(100);
-plot(x, y, 'k--', 'LineWidth', 0.3, 'HandleVisibility', 'off')
-hold on
+% plot(x, y, 'k--', 'LineWidth', 0.5, 'HandleVisibility', 'off')
+% hold on
 
-tl = 10:10:100;
+tl = 0:5:100;
 ticklabels = cellstr(num2str(tl'));
 
-for i = 1:3
+for i = 1:4
     %{
         Reads and plots force and extension data from Instron's raw data (.csv)
     %}
@@ -34,24 +40,24 @@ for i = 1:3
     hold on
 end
 
-title({"Grip Force Test";'HSA Rotation = 90\circ'})
+title({"Grip Force Test";'For HSA Rotation of 90\circ and 120\circ'})
 xlabel('Extension [mm]')
-ylabel('Force [N]')
-xlim([0 100])
-ylim([-6 12])
+ylabel('Normal Force, F_g [N]')
+xlim([0 50]) % extension scale
+ylim([0 13]) % force scale
 
 xticks(tl)
 xticklabels(ticklabels)
 
 grid on
-legend('Location', 'southwest')
+legend('Location', 'south')
 
 
 % figure size
 
 x0 = 900;
 y0 = 410;
-width = 450;
+width = 400;
 height = 400;
 set(gcf, 'position', [x0, y0, width, height])
 exportgraphics(gcf, 'D:\Srivatsan\HSA-gripper-files\Plot Images\GripForce.png', 'Resolution', 800)
